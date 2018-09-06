62cd2b51c4f8c6dbf43cae1ce5296b65d2b408e5
troychuinard <tccpg288@gmail.com>
2018-09-05 20:26:30 -0500

    Build for Submission

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
@@ -60,6 +60,7 @@ public class ArticleListActivity extends ActionBarActivity implements
         setContentView(R.layout.activity_article_list);
 
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
 
 
 
